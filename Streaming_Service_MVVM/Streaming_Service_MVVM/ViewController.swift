832e2fe9f254cdbceb109262e68cd1bfdc14399d
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-08-13 18:32:48 +0500

    Add project.

---
+++ Streaming_Service_MVVM/Streaming_Service_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Streaming_Service_MVVM
+//
+//  Created by Daniil Yarkovenko on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
