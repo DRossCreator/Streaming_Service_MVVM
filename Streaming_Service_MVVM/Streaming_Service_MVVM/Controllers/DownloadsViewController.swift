b7a7cd8bbece07f68cd06128444f364476163a3b
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-08-14 12:44:49 +0500

    completeTabbar.

--- Streaming_Service_MVVM/Streaming_Service_MVVM/Controllers/DownloadsViewController.swift
+++ Streaming_Service_MVVM/Streaming_Service_MVVM/Controllers/DownloadsViewController.swift
@@ -12,6 +12,6 @@ class DownloadsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .purple
+        view.backgroundColor = .systemBackground
     }
 }
