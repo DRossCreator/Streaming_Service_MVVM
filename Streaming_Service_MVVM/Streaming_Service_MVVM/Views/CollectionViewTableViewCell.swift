dce16b24ea6a3213163920e3e1f5880b8dceb706
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-08-14 13:28:09 +0500

    Add TableView with Collection inside.

---
+++ Streaming_Service_MVVM/Streaming_Service_MVVM/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Streaming_Service_MVVM
+//
+//  Created by Daniil Yarkovenko on 14.08.2022.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    //MARK: - Singleton
+
+    static let identifier = "CollectionViewTableViewCell"
+
+    //MARK: - Views
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 200, height: 140)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
+        return collectionView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+}
+
+//MARK: - UIcollectionViewDelegate
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate {
+
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension CollectionViewTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+
+
+}
+
+//MARK: -
+
+//extension CollectionViewTableViewCell:
---
+++ Streaming_Service_MVVM/Streaming_Service_MVVM/Views/Controllers/HomeViewController.swift
@@ -0,0 +1,72 @@
+//
+//  HomeViewController.swift
+//  Streaming_Service_MVVM
+//
+//  Created by Daniil Yarkovenko on 13.08.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    //MARK: - Views
+
+    private lazy var homeFeedTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+    }()
+
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+
+        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+
+}
+
+//MARK: - UITableViewDelegate
+
+extension HomeViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
+    }
+}
+
+//MARK: - UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        20
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else { return UITableViewCell() }
+
+        return cell
+    }
+
+
+}
